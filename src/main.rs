ba177f01e540b417d22dce86fcd8460c6b82e8fe
Finn Coffey <finn.coffey@gmail.com>
2022-06-05 13:33:50 +1000

    Add basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use ggez::*;
+
+#[derive(Default)]
+struct State {}
+
+impl event::EventHandler for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, graphics::Color::from_rgb(255, 127, 0));
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let state = State::default();
+
+    let (ctx, event_loop) = ContextBuilder::new("arms", "soxfox42")
+        .window_setup(conf::WindowSetup::default().title("The Arms"))
+        .window_mode(conf::WindowMode::default().dimensions(1200.0, 800.0))
+        .build()
+        .unwrap();
+    event::run(ctx, event_loop, state);
 }
