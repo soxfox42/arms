64a2a8160c966b6a374b31de66a68bf5581596bd
Finn Coffey <finn.coffey@gmail.com>
2022-06-05 13:50:02 +1000

    Draw line

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
 use ggez::*;
+use glam::Vec2;
 
-#[derive(Default)]
-struct State {}
+struct State {
+    pos: Vec2,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { pos: Vec2::ZERO }
+    }
+}
 
 impl event::EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let mouse_pos: glam::Vec2 = input::mouse::position(ctx).into();
+        self.pos = mouse_pos.normalize_or_zero() * 200.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, graphics::Color::from_rgb(255, 127, 0));
+        graphics::clear(ctx, graphics::Color::BLACK);
+        if self.pos != Vec2::ZERO {
+            let line = graphics::Mesh::new_line(
+                ctx,
+                &[Vec2::ZERO, self.pos],
+                10.0,
+                graphics::Color::from_rgb(255, 0, 127),
+            )?;
+            graphics::draw(ctx, &line, graphics::DrawParam::default())?;
+        }
         graphics::present(ctx)?;
         Ok(())
     }
 }
 
 fn main() {
-    let state = State::default();
+    let state = State::new();
 
     let (ctx, event_loop) = ContextBuilder::new("arms", "soxfox42")
         .window_setup(conf::WindowSetup::default().title("The Arms"))
