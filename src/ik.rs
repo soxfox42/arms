48c5b6b805254cac3b8ece2dfc4ac2d48ae00a02
Finn Coffey <finn.coffey@gmail.com>
2022-06-05 17:52:49 +1000

    IK

--- src/ik.rs
+++ src/ik.rs
@@ -17,35 +17,48 @@ impl Arms {
         }
     }
 
-    fn end_pos(&self) -> Vec2 {
-        Vec2::ZERO
+    pub fn set_arm_length(&mut self, arm_len: f32) {
+        self.lengths = vec![arm_len; self.count]
+    }
+
+    fn positions(&self) -> Vec<Vec2> {
+        let mut current = Vec2::ZERO;
+        let mut current_angle = 0.0;
+        let mut positions = vec![current];
+
+        for (&angle, &length) in self.angles.iter().zip(&self.lengths) {
+            current_angle += angle;
+            let offset = Vec2::new(current_angle.cos(), current_angle.sin()) * length;
+            current += offset;
+            positions.push(current);
+        }
+
+        positions
     }
 
     pub fn update_angles(&mut self, target: Vec2) {
         for _step in 0..MAX_IK_STEPS {
             for arm in 0..self.count {
-                self.angles[arm] += arm as f32 / 1000.0 + 0.001;
+                let positions = self.positions();
+                let pivot_pos = positions[arm];
+                let end_pos = positions[self.count];
+                if end_pos.distance(target) < 0.1 {
+                    return;
+                }
+
+                let to_end = end_pos - pivot_pos;
+                let to_target = target - pivot_pos;
+                self.angles[arm] += to_end.angle_between(to_target);
             }
         }
     }
 
     pub fn arms(&self) -> Vec<(Vec2, Vec2)> {
-        let mut current = Vec2::ZERO;
-        let mut positions = vec![current];
-        
-        for (&angle, &length) in self.angles.iter().zip(&self.lengths) {
-            let offset = Vec2::new(angle.cos(), angle.sin()) * length;
-            current += offset;
-            positions.push(current);
-        }
-
         let mut arms = Vec::new();
-        for arm in positions.windows(2) {
+        for arm in self.positions().windows(2) {
             arms.push((arm[0], arm[1]));
         }
 
         arms
     }
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ use glam::Vec2;
 struct State {
     arms: ik::Arms,
     colors: Vec<graphics::Color>,
+    size: Vec2,
 }
 
 impl State {
-    fn new(radius: f32) -> Self {
+    fn new() -> Self {
+        let size = Vec2::new(1200.0, 800.0);
+        let radius = (size.min_element() - 50.0) / 2.0;
+
         Self {
             arms: ik::Arms::new(3, radius / 3.0),
             colors: vec![
@@ -17,13 +21,27 @@ impl State {
                 graphics::Color::from_rgb(255, 0, 127),
                 graphics::Color::from_rgb(0, 127, 255),
             ],
+            size,
         }
     }
 }
 
 impl event::EventHandler for State {
+    fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
+        let radius = (f32::min(width, height) - 50.0) / 2.0;
+        self.arms.set_arm_length(radius / 3.0);
+
+        self.size = Vec2::new(width, height);
+
+        graphics::set_screen_coordinates(
+            ctx,
+            graphics::Rect::new(-width / 2.0, -height / 2.0, width, height),
+        )
+        .expect("Failed to update screen coordinates");
+    }
+
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        let mouse_pos = input::mouse::position(ctx).into();
+        let mouse_pos = Vec2::from(input::mouse::position(ctx)) - self.size / 2.0;
         self.arms.update_angles(mouse_pos);
         Ok(())
     }
@@ -31,12 +49,7 @@ impl event::EventHandler for State {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, graphics::Color::BLACK);
 
-        let size: Vec2 = graphics::size(&ctx).into();
-        let min_size = size.min_element();
-        graphics::set_screen_coordinates(
-            ctx,
-            graphics::Rect::new(-size.x / 2.0, -size.y / 2.0, size.x, size.y),
-        )?;
+        let min_size = self.size.min_element();
 
         let stroke_options = graphics::StrokeOptions::default()
             .with_line_width(min_size / 50.0)
@@ -51,13 +64,14 @@ impl event::EventHandler for State {
             )?;
             graphics::draw(ctx, &line, graphics::DrawParam::default())?;
         }
+
         graphics::present(ctx)?;
         Ok(())
     }
 }
 
 fn main() {
-    let (ctx, event_loop) = ContextBuilder::new("arms", "soxfox42")
+    let (mut ctx, event_loop) = ContextBuilder::new("arms", "soxfox42")
         .window_setup(
             conf::WindowSetup::default()
                 .title("The Arms")
@@ -71,10 +85,13 @@ fn main() {
         .build()
         .unwrap();
 
-    let size: Vec2 = graphics::size(&ctx).into();
-    let radius = (size.min_element() - 50.0) / 2.0;
+    graphics::set_screen_coordinates(
+        &mut ctx,
+        graphics::Rect::new(-600.0, -400.0, 1200.0, 800.0),
+    )
+    .expect("Failed to update screen coordinates");
 
-    let state = State::new(radius);
+    let state = State::new();
 
     event::run(ctx, event_loop, state);
 }
